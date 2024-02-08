06929008f643891ef8af4f54c486af8c0ad50196
isromero <ismaromeroortega@gmail.com>
2024-02-08 21:43:52 +0100

    Create Root endpoint

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} at http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Salesforce Node.js Integration');
+});
