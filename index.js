b05cd2fc38c1b2d73c2b3d42aa1df7c3e4e52141
isromero <ismaromeroortega@gmail.com>
2024-02-08 21:51:43 +0100

    Installation dotenv and ready environment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
@@ -10,3 +13,6 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.send('Salesforce Node.js Integration');
 });
+
+
+const { SF_LOGIN_URL, SF_USERNAME, SF_PASSWORD, SF_SECURITY_TOKEN } = process.env;
